94311251a10e1b85b7ad9e5a34fb114746c6eb05
Doug Johnson <finson@whidbey.com>
2016-05-09 23:06:49 -0700

    Move unfinished drivers out of the way while refreshing tests.

--- src/DXStepper/LUStepper.cpp
+++
@@ -1,8 +0,0 @@
-#include "LUStepper.h"
-
-LUStepper::LUStepper(byte interface, int steps_per_rev,
-                     byte pin1, byte pin2, byte pin3, byte pin4) :
-    AsyncStepper(interface, steps_per_rev, pin1, pin2, pin3, pin4)
-{ }
-
-LUStepper::~LUStepper() {}
