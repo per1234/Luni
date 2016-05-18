698947ef4f8674d5c3cbfd38cddd62dd4d6a62ba
Doug Johnson <finson@whidbey.com>
2016-05-18 16:31:17 -0700

    Move tests to LuniTest repo to avoid conflict and confusion.

--- src/TestFrame/DeviceDriverTest.cpp
+++
@@ -1,7 +0,0 @@
-#include <LuniLib.h>
-#include "DeviceDriverTest.h"
-
-DeviceDriverTest::DeviceDriverTest(const char *group, const char *unit) {
-  groupID = (char *)group;
-  unitID = (char *)unit;
-}
--- src/TestFrame/Tools.cpp
+++
@@ -1,7 +0,0 @@
-#include "Tools.h"
-
-int ToolsClass::freeRam () {
-  extern int __heap_start, *__brkval;
-  int v;
-  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
-}
